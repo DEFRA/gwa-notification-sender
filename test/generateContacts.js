9da76159cf546be20d53a44b404a8df53c761c37
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-04-20 10:37:01 +0100

    refactor: export anonymous functions

--- test/generateContacts.js
+++ test/generateContacts.js
@@ -1,10 +1,8 @@
-function generateContacts (n) {
+module.exports = (count) => {
   const contacts = []
   let numberBase = 100000000
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < count; i++) {
     contacts.push({ phoneNumber: `07${numberBase++}` })
   }
   return contacts
 }
-
-module.exports = generateContacts
--- test/generateMessageItems.js
+++ test/generateMessageItems.js
@@ -1,9 +1,10 @@
 module.exports = (count) => {
   const messageItems = []
+  let numberBase = 100000000
   for (let i = 0; i < count; i++) {
     messageItems.push({
       messageId: `messageId-${i}`,
-      messageText: Buffer.from(JSON.stringify({ notification: { phoneNumber: `07${i}`, message: `message-${i}` } }), 'utf8').toString('base64'),
+      messageText: Buffer.from(JSON.stringify({ notification: { phoneNumber: `07${numberBase++}`, message: `message-${i}` } }), 'utf8').toString('base64'),
       popReceipt: `popReceipt-${i}`
     })
   }
