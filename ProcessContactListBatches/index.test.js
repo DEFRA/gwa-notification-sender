e8974a0a7d07266604c2224d5432f0aa0fc19f1a
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-04-28 17:14:12 +0100

    refactor: simplify mocking in tests

--- ProcessContactListBatches/index.test.js
+++ ProcessContactListBatches/index.test.js
@@ -1,48 +1,68 @@
-const { Readable } = require('stream').Stream
-
-const { mockBlobClient, mockContainerClient, mockDelete, mockDownload } = require('@azure/storage-blob').mocks
-
-const context = require('../test/defaultContext')
 const testEnvVars = require('../test/testEnvVars')
-const generateContacts = require('../test/generateContacts')
-
-const processContactListBatches = require('.')
-const { bindings: functionBindings } = require('./function')
 
 const inputBindingName = 'contactListBatchFileName'
 const outputBindingName = 'messagesToSend'
 
-function testSentMessages (fileContents, sentMessages) {
-  const { contacts, message } = fileContents
-  sentMessages.forEach((sentMessage, idx) => {
-    expect(sentMessage.message).toEqual(message)
-    expect(sentMessage.phoneNumber).toEqual(contacts[idx].phoneNumber)
-  })
-}
-
-function setMockDownload (contents, encoding) {
-  const mockReadable = new Readable({ read () {} })
-  if (encoding) {
-    mockReadable.setEncoding(encoding) // default is null
-  }
-  mockReadable.push(JSON.stringify(contents))
-  mockReadable.push(null)
-  mockDownload.mockImplementation(() => { return { readableStreamBody: mockReadable } })
-}
-
 describe('ProcessContactListBatches function', () => {
-  beforeAll(() => { context.bindings[inputBindingName] = inputBindingName })
+  const { Readable } = require('stream').Stream
 
-  afterEach(() => { jest.clearAllMocks() })
+  const context = require('../test/defaultContext')
+  const generateContacts = require('../test/generateContacts')
 
-  test('file specified in message is downloaded', async () => {
-    setMockDownload({ contacts: [], message: 'messages' })
+  function testSentMessages (fileContents, sentMessages) {
+    const { contacts, message } = fileContents
+    sentMessages.forEach((sentMessage, idx) => {
+      expect(sentMessage.message).toEqual(message)
+      expect(sentMessage.phoneNumber).toEqual(contacts[idx].phoneNumber)
+    })
+  }
 
+  function setMockDownload (contents, encoding) {
+    const mockReadable = new Readable({ read () {} })
+    if (encoding) {
+      mockReadable.setEncoding(encoding) // default is null
+    }
+    mockReadable.push(JSON.stringify(contents))
+    mockReadable.push(null)
+
+    const deleteBlobMock = jest.fn().mockResolvedValue()
+    ContainerClient.prototype.getBlobClient.mockImplementation(() => {
+      return {
+        download: jest.fn().mockResolvedValue({ readableStreamBody: mockReadable }),
+        delete: deleteBlobMock
+      }
+    })
+    return {
+      deleteBlobMock
+    }
+  }
+
+  const inputFileName = 'incoming-file.json'
+
+  let ContainerClient
+  let processContactListBatches
+
+  beforeEach(() => {
+    jest.mock('@azure/storage-blob')
+    jest.clearAllMocks()
+    jest.resetModules()
+    ContainerClient = require('@azure/storage-blob').ContainerClient
+    processContactListBatches = require('.')
+
+    context.bindings[inputBindingName] = inputFileName
+  })
+
+  test.only('file specified in message is downloaded and deleted', async () => {
+    const contents = { contacts: [], message: 'messages' }
+    const { deleteBlobMock } = setMockDownload(contents)
     await processContactListBatches(context)
 
-    expect(mockContainerClient).toHaveBeenCalledTimes(1)
-    expect(mockContainerClient).toHaveBeenCalledWith(testEnvVars.AzureWebJobsStorage, testEnvVars.CONTACT_LIST_BATCHES_CONTAINER)
-    expect(mockDownload).toHaveBeenCalledTimes(1)
+    expect(ContainerClient).toHaveBeenCalledTimes(1)
+    expect(ContainerClient).toHaveBeenCalledWith(testEnvVars.AzureWebJobsStorage, testEnvVars.CONTACT_LIST_BATCHES_CONTAINER)
+    const containerClientMockInstance = ContainerClient.mock.instances[0]
+    expect(containerClientMockInstance.getBlobClient).toHaveBeenCalledTimes(1)
+    expect(containerClientMockInstance.getBlobClient).toHaveBeenCalledWith(inputFileName)
+    expect(deleteBlobMock).toHaveBeenCalledTimes(1)
   })
 
   test('a message is sent for a single contact in the batch', async () => {
@@ -52,9 +72,9 @@ describe('ProcessContactListBatches function', () => {
 
     await processContactListBatches(context)
 
-    expect(context.bindings).toHaveProperty('messagesToSend')
-    expect(context.bindings.messagesToSend).toHaveLength(1)
-    const messageSent = context.bindings.messagesToSend[0]
+    expect(context.bindings).toHaveProperty(outputBindingName)
+    expect(context.bindings[outputBindingName]).toHaveLength(1)
+    const messageSent = context.bindings[outputBindingName][0]
     expect(messageSent.message).toEqual(message)
     expect(messageSent.phoneNumber).toEqual(phoneNumber)
   })
@@ -68,31 +88,21 @@ describe('ProcessContactListBatches function', () => {
 
     await processContactListBatches(context)
 
-    expect(context.bindings).toHaveProperty('messagesToSend')
-    const sentMessages = context.bindings.messagesToSend
+    expect(context.bindings).toHaveProperty(outputBindingName)
+    const sentMessages = context.bindings[outputBindingName]
     expect(sentMessages).toHaveLength(numberOfContacts)
 
     testSentMessages(rawFileContents, sentMessages)
   })
 
-  test('batch contact list blob is deleted', async () => {
-    setMockDownload({ contacts: [], message: 'messages' })
-
-    await processContactListBatches(context)
-
-    expect(mockBlobClient).toHaveBeenCalledTimes(1)
-    expect(mockBlobClient).toHaveBeenCalledWith(inputBindingName)
-    expect(mockDelete).toHaveBeenCalledTimes(1)
-  })
-
   test('file downloads when contents are utf8 encoded', async () => {
-    setMockDownload({ contacts: [], message: 'messages' }, 'utf8')
+    const { deleteBlobMock } = setMockDownload({ contacts: [], message: 'messages' }, 'utf8')
 
     await processContactListBatches(context)
 
-    expect(mockBlobClient).toHaveBeenCalledTimes(1)
-    expect(mockBlobClient).toHaveBeenCalledWith(inputBindingName)
-    expect(mockDelete).toHaveBeenCalledTimes(1)
+    expect(ContainerClient).toHaveBeenCalledTimes(1)
+    expect(ContainerClient).toHaveBeenCalledWith(inputFileName)
+    expect(deleteBlobMock).toHaveBeenCalledTimes(1)
   })
 
   test('an error is thrown (and logged) when an error occurs', async () => {
@@ -105,6 +115,8 @@ describe('ProcessContactListBatches function', () => {
 })
 
 describe('ProcessContactListBatches bindings', () => {
+  const { bindings: functionBindings } = require('./function')
+
   test('queueTrigger input binding is correct', () => {
     const bindings = functionBindings.filter(binding => binding.direction === 'in')
     expect(bindings).toHaveLength(1)
