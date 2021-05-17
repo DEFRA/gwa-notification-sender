4ae8731c6837b7576f2dfc30ce2244effafe9c93
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-05-17 18:07:42 +0100

    feat: update receipts according to message send status

--- SendMessage/index.js
+++ SendMessage/index.js
@@ -14,21 +14,41 @@ const notifyTemplateId = process.env.NOTIFY_TEMPLATE_ID
 const notifyClient = new NotifyClient(notifyClientApiKey)
 
 function isErrorOkToTryAgain (error) {
-  return ['EAI_AGAIN', 'ECONNRESET', 'ENOTFOUND', 'ETIMEDOUT', 409].includes(error?.code) || [403].includes(error?.status_code)
+  return ['EAI_AGAIN', 'ECONNRESET', 'ENOTFOUND', 'ETIMEDOUT'].includes(error?.code) || [403].includes(error?.status_code)
+}
+
+function isErrorConflict (error) {
+  return error?.code === 409
 }
 
 function isRateLimitExceeded (error) {
   return error?.status_code === 429
 }
 
+function warnAndThrow (context, notification, e) {
+  context.log.warn('Message sending has failed but is ok to try again', notification)
+  // Throwing error causes the message to be added back to the queue with
+  // dequeueCount incremented (happens automatically)
+  throw new Error(e)
+}
+
+async function replaceReceipt (receipt, status) {
+  receipt.status = `Internal: ${status}`
+  await receiptsContainer.item(receipt.id).replace(receipt)
+}
+
 module.exports = async function (context) {
   const { notification } = context.bindings
   context.log('Message To Send Queue Trigger function activated for:', notification)
 
+  let receipt
+
   try {
     const { message, phoneNumber } = notification
     const reference = uuid()
-    await receiptsContainer.items.create({ id: reference, status: 'Sent to Notify', to: phoneNumber })
+    receipt = { id: reference, status: 'Internal: Sent to Notify', to: phoneNumber }
+
+    await receiptsContainer.items.create(receipt)
 
     await notifyClient.sendSms(notifyTemplateId, phoneNumber, {
       personalisation: { message },
@@ -40,6 +60,7 @@ module.exports = async function (context) {
     context.log.error(error)
 
     if (isRateLimitExceeded(error)) {
+      await replaceReceipt(receipt, 'Rate limit exceeded')
       // Do not rethrow, move to rateLimitExceeded queue.
       context.bindings.rateLimitExceeded = {
         error,
@@ -50,11 +71,13 @@ module.exports = async function (context) {
       // (default 5). We don't want to use poision queue as it doesn't include
       // the error message so add to failed queue.
       if (dequeueCount < 5 && isErrorOkToTryAgain(error)) {
-        context.log.warn('Message sending has failed but is ok to try again', notification)
-        // Throwing error causes the message to be added back to the queue with
-        // dequeueCount incremented (happens automatically)
-        throw new Error(e)
+        await replaceReceipt(receipt, 'To be retried')
+        warnAndThrow(context, notification, e)
+      } else if (isErrorConflict(error)) {
+        await replaceReceipt(receipt, 'Conflict')
+        warnAndThrow(context, notification, e)
       } else {
+        await replaceReceipt(receipt, 'Failed to send')
         context.log.warn('add to failed queue')
         // Add to failed queue for later analysis, no auto reprocessing
         context.bindings.failed = {
